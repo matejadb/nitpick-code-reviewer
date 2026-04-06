8c16dcbc318aaed820782f2fae1bf32f4e13792e
matejadb <92729211+matejadb@users.noreply.github.com>
2026-04-06 17:42:32 +0200

    feat: add email input field

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import Logo from '../components/Logo';
 function LoginPage() {
 	return (
 		<div className="bg-neutral-700 min-h-screen flex flex-col items-center justify-center">
-			<div className=" bg-neutral-950 max-w-135 flex flex-col gap-4 p-12 rounded-2xl items-center">
+			<div className=" bg-neutral-950 max-w-135 flex flex-col gap-4 p-12 rounded-2xl items-center text-center">
 				<Logo />
 
 				<div className="flex flex-col gap-2 items-center">
@@ -14,6 +14,25 @@ function LoginPage() {
 						Please log in to continue
 					</p>
 				</div>
+
+				<form className="pt-6 flex flex-col gap-4">
+					<div className="flex flex-col gap-1.5">
+						<label
+							className="font-inter text-left  text-neutral-0 font-medium text-sm leading-[1.2] tracking-[-0.2]"
+							htmlFor="mail">
+							Email Address
+						</label>
+						<div className="border border-neutral-600 rounded-lg relative">
+							<input
+								type="email"
+								name="mail"
+								id="mail"
+								className="font-inter font-medium text-sm w-full text-neutral-500 px-4 py-3"
+								placeholder="email@example.com"
+							/>
+						</div>
+					</div>
+				</form>
 			</div>
 		</div>
 	);
