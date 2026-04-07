2d298d8203feecfc831a75b396422511a04d7983
matejadb <92729211+matejadb@users.noreply.github.com>
2026-04-07 14:51:26 +0200

    feat: add form boilerplate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import RegisterPage from './pages/RegisterPage';
-import LoginPage from './pages/LoginPage';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-	return (
-		<>
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/register" element={<RegisterPage />} />
-				<Route path="/login" element={<LoginPage />} />
-			</Routes>
-		</>
-	);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,4 +1,7 @@
+import { Link } from "react-router-dom";
+
 import Logo from "../components/Logo";
+import Eye from "/assets/icon-show-password.svg";
 
 function LoginPage() {
   return (
@@ -6,7 +9,7 @@ function LoginPage() {
       <div className="flex flex-col items-center gap-4 rounded-2xl border border-neutral-800 bg-neutral-950 px-4 py-12">
         <Logo width={23.75} height={7} />
 
-        <div className="flex flex-col items-center gap-2">
+        <div className="flex flex-col items-center gap-2 text-center">
           <h2 className="text-neutral-0 font-inter text-2xl leading-[1.2] font-bold tracking-[-0.5px]">
             Welcome to Nitpick
           </h2>
@@ -15,10 +18,10 @@ function LoginPage() {
           </p>
         </div>
 
-        <form className="flex flex-col gap-4 pt-6">
+        <form className="flex w-full flex-col gap-4 pt-6">
           {/* Email Input */}
-          <div>
-            <div>
+          <div className="flex w-full flex-col gap-1.5">
+            <div className="flex items-center justify-between">
               <label
                 htmlFor="mail"
                 className="font-inter text-neutral-0 text-sm leading-[1.2] font-medium tracking-[-0.2px]"
@@ -26,8 +29,59 @@ function LoginPage() {
                 Email Address
               </label>
             </div>
-            <div></div>
+            <div className="flex overflow-hidden rounded-lg border border-neutral-600 outline-neutral-600 has-focus:outline-2 has-focus:outline-offset-3">
+              <input
+                type="email"
+                id="mail"
+                name="mail"
+                className="font-inter text-neutral-0 w-full flex-1 px-4 py-3 text-sm leading-[1.3] font-normal tracking-[-0.2px] placeholder-neutral-500 focus:outline-none"
+                placeholder="email@example.com"
+              />
+            </div>
           </div>
+
+          {/* Password Input */}
+          <div className="flex w-full flex-col gap-1.5">
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="font-inter text-neutral-0 text-sm leading-[1.2] font-medium tracking-[-0.2px]"
+              >
+                Password
+              </label>
+
+              <Link
+                to="/"
+                className="font-inter text-[12px] leading-[1.4] font-normal text-neutral-400 underline underline-offset-1"
+              >
+                Forgot
+              </Link>
+            </div>
+            <div className="flex overflow-hidden rounded-lg border border-neutral-600 outline-neutral-600 has-focus:outline-2 has-focus:outline-offset-3">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="font-inter text-neutral-0 w-full flex-1 px-4 py-3 text-sm leading-[1.3] font-normal tracking-[-0.2px] placeholder-neutral-500 focus:outline-none"
+              />
+
+              <button className="cursor-pointer">
+                <img
+                  src={Eye}
+                  alt="Show password"
+                  className="bg-neutral-500 fill-neutral-500"
+                />
+              </button>
+            </div>
+          </div>
+
+          {/* Submit Button */}
+          <button
+            type="submit"
+            className="font-inter text-neutral-0 rounded-lg bg-blue-500 px-4 py-3 text-[16px] leading-[1.2] font-semibold tracking-[-0.3px]"
+          >
+            Login
+          </button>
         </form>
       </div>
     </div>
